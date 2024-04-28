911c4a49668586d9c5912f3e6ca8f95177989507
Vincenius <vincent.will@vaitrade.de>
2024-04-28 10:15:46 +0200

    set session to empty

--- src/pages/api/logout.js
+++ src/pages/api/logout.js
@@ -2,6 +2,7 @@ import { getIronSession } from 'iron-session';
 
 export default async function handler(req, res) {
   const session = await getIronSession(req, res, { password: process.env.SESSION_PASSWORD, cookieName: process.env.SESSION_KEY, ttl: 0 });
+  session = {}
   session.destroy()
   await session.save()
 
