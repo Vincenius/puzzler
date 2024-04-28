5ce9b61877f8c4c0f34fb8ddb2bd8a0c2b29b6cb
Vincenius <vincent.will@vaitrade.de>
2024-04-28 10:16:53 +0200

    use let for session

--- src/pages/api/logout.js
+++ src/pages/api/logout.js
@@ -1,7 +1,7 @@
 import { getIronSession } from 'iron-session';
 
 export default async function handler(req, res) {
-  const session = await getIronSession(req, res, { password: process.env.SESSION_PASSWORD, cookieName: process.env.SESSION_KEY, ttl: 0 });
+  let session = await getIronSession(req, res, { password: process.env.SESSION_PASSWORD, cookieName: process.env.SESSION_KEY, ttl: 0 });
   session = {}
   session.destroy()
   await session.save()
